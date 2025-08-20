60c11fdfc921f30c7ea1f38e3af2d25a538d0bf0
phongndps37996 <phongndps37996@gmail.com>
2025-08-20 12:31:07 +0700

    fix bundle

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,7 @@ async function bootstrap() {
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
     ],
+    customSiteTitle: 'API Documentation',
   });
   await app.listen(process.env.PORT ?? 5000);
 }
