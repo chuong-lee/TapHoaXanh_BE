a6cc62dbb8871e86ca7d415052775200641f9ea1
phongndps37996 <phongndps37996@gmail.com>
2025-08-25 21:42:23 +0700

    fix config3

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
